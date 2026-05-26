9f9edfcadd7a357c5de9a37eb239483677e5113e
RaKostas <136069329+RaKostas@users.noreply.github.com>
2026-05-26 15:31:51 +0200

    Add 10-Fold Cross Validation for linear regression in R.r

--- Projects/Big Data/2nd/10-Fold Cross Validation in R.r
+++ Projects/Big Data/2nd/10-Fold Cross Validation in R.r
@@ -1,26 +1,11 @@
 setwd("C:\\Users\\user\\Desktop")
-
-## Διασταυρωμένη Επικύρωση k-Πτυχών
-# Συνάρτηση που υπολογίζει και επιστρέφει το Μέσο Τετραγωνικό Σφάλμα (Root Mean Squared Error 
-# - RMSE) 
-# predictedValues: διάνυσμα με τιμές της εξαρτημένης μεταβλητής που προβλέπει το μοντέλο 
-#                  παλινδρόμησης
-# actualValues: διάνυσμα με τις πραγματικές τιμές της εξαρτημένης με-ταβλητής
+# Διασταυρωμένη Επικύρωση k-Πτυχών
 # Επιστρέφει το Μέσο Τετραγωνικό Σφάλμα πρόβλεψης
 calculateRMSE<-function(predictedValues, actualValues){
   err<- sqrt( mean((actualValues - predictedValues)^2)  )
   return( err )
 }
 
-
-# Συνάρτηση που υλοποιεί τον αλγόριθμο της διασταρωμένης επικύρωσης k-πτυχών.
-# Η συνάρτηση κάνει χρήση του μέσου τετραγωνικού σφάλματος (RMSE) ως μετρική σφάλματος.
-# Παράμετροι συνάρτησης:
-# data: το σύνολο δεδομένων που θα χωριστεί σε τμήματα ελέγχουν κα εκπαίδευσης
-# frml: το γραμμικό μοντέλο παλινδρόμησης που θα αξιολογηθεί η ακρί-βεια πρόβλεψής του
-# k: η τιμή k της διασταυρωμένης επικύρωσης k-πτυχών που δηλώνει σε πόσα τμήματα θα διαχωριστεί
-#    το αρχικό σύνολο δεδομένων
-# Επιστρεφόμενη τιμή:
 # Η συνάρτηση επιστρέφει τον μέσο όρο του μέσου τετραγωνικού σφάλματος
 kFoldCrossValidation<-function(data, frml, k){
   # Τυχαία αναφιάταξη των παρατηρήσεων του συνόλου δεδομένων
@@ -30,8 +15,6 @@ kFoldCrossValidation<-function(data, frml, k){
   folds <- cut(seq(1,nrow(dataset)), breaks=k, labels=FALSE)
   # Διάνυσμα όπου αποθηκεύεται το Μέσο Τε
   RMSE<-vector()
-  # Επαναλαηπτική διαδικασία όπου κάθε ένα από τα k τμήματα θα χρησι-μοποιηθεί διαδοχικά
-  # ως σύνολο ελέγχου για το μοντέλο παλινδρόμησης και όλα τα υπόλοιπα ως σύνολο εκπαίδευσης. 
   # Η διαδικασία θα τερματίσει εάν όλα τα τμήματα έχουν χρησιμοποιηθεί ως σύνολο σλέγχου.
   for(i in 1:k){
     # Καθορισμός του τμήματος ελέγχου για την τρέχουσα επανάληψη 
@@ -51,11 +34,11 @@ kFoldCrossValidation<-function(data, frml, k){
     # Αποθήκευση τιμής σφάλματος
     RMSE<-c(RMSE, error)
   }
+  
   # Επιστροφή μέσης τιμής των σφαλμάτων που προέκυψαν απ'όλα τα τμήμα-τα ελέγχου
   return( mean(RMSE) )
 }
-
-#i)
+#ii)
 # Dataset με γεωγραφικά και μετεωρολογικά σοιχεια για πυρκαγιές που εκδηλώθηκαν στην Πορτογαλία
 forestfires<-read.csv("forestfires.csv", sep=",", header=T,stringsAsFactors = F, quote = "\"")
 
@@ -64,16 +47,23 @@ forestfires<-read.csv("forestfires.csv", sep=",", header=T,stringsAsFactors = F,
 # σε περίπτωση που μια τουλάχιστον μεταβλητή έχει missing value
 forestfires<-na.omit(forestfires)
 
-
-# Το  υποψήφιο μοντέλο των οποίων θα αξιολογηθεί η ικανότητα πρόβλεψης της 
+# Τα  υποψήφια μοντέλα των οποίων θα αξιολογηθεί η ικανότητα πρόβλεψης της 
 #επιφάνειας που θα καεί βάσει των μετεωρολογικών συνθηκών που επικρατούν με τη μέθοδο της 
 #διασταυρωτικής επικύρωσης 10-φορές. 
 #area=β1temp +β2wind+β3rain +βο
-# Το μοντέλο παλινδρόμησης αποθηκεύεται στο διάνυσμα ως συμβολοσειρά και θα μετατραπεί
+# Τα μοντέλα παλινδρόμησης αποθηκεύονται στο διάνυσμα ως συμβολοσειρά και θα μετατραπούν
 # σε τύπο (formula) της R
 predictionModels<-vector()
 predictionModels[1]<-"area ~ temp+wind+rain"
+forestfires2<-forestfires[which(forestfires$area<3.2),]
+forestfires2<-na.omit(forestfires2)
+predictionModels[2]<-"area ~ temp+wind+rain"
+# Μετά από κάθε διασταυρωμένη επικύρωση, ο μέσος όρος του μέσου τετραγωνικού 
+# σφάλματος κάθε μοντέλου θα αποθηκευτεί σε διάνυσμα.
 modelMeanRMSE<-vector()
+modelMeanRMSE2<-vector()
+#Διασταυρωμένη επικύρωση 10-φορές για κάθε ένα από τα 
+# δύο υποψήφια μοντέλα.
 for (k in 1:length(predictionModels[1])){
   # Δισταυρωμένη επικύρωση 10-πτυχών για το γραμμικό μοντέλο παλινδρόμησης k
   modelErr<-kFoldCrossValidation(forestfires,as.formula(predictionModels[k]), 10)
@@ -82,26 +72,22 @@ for (k in 1:length(predictionModels[1])){
   print( sprintf("Linear regression model [%s]: prediction error [%f]", predictionModels[k], modelErr ) )
 }
 
-
-# To μοντέλο παλινδρόμηνσης με το μέσο τετραγωνικό σφάλμα χρησιμοποιώντας την μέθοδο OLS,όπου
-#εκτιμώνται οι συντελεστές του λαμβάνοντας υπόψη ολόκληρο το σύνολο δεδομένων ως σύνολο εκπαίδευσης
-linear.model<-lm( as.formula(predictionModels[k]), data=forestfires )
-linear.model
-
-#ii)
-#Τιμή της μεταβλητής area μικρότερη από 3.2 εκτάρια
-forestfires2<-forestfires[which(forestfires$area<3.2),]
-forestfires2<-na.omit(forestfires2)
-predictionModels[2]<-"area ~ temp+wind+rain"
-modelMeanRMSE2<-vector()
 for (k in 1:length(predictionModels[2])){
-  # Δισταυρωμένη επικύρωση 10-πτυχών για το γραμμικό μοντέλο παλιν-δρόμησης k
+  # Δισταυρωμένη επικύρωση 10-πτυχών για το γραμμικό μοντέλο παλινδρόμησης k
   modelErr2<-kFoldCrossValidation(forestfires2, as.formula(predictionModels[k]), 10)
   #Αποθήκευση του μέσου σφάλματος
   modelMeanRMSE2<-c(modelMeanRMSE2, modelErr2)
   print( sprintf("Linear regression model [%s]: prediction error [%f]", predictionModels[k], modelErr2 ) )
 }
-#To δεύτερο μοντέλο παλινδρόμηνσης με το μέσο τετραγωνικό σφάλμα χρησιμοποιώντας την μέθοδο OLS,όπου
+#μέσο τετραγωνικό σφάλμα των δυο μοντέλων
+all<-c(modelMeanRMSE,modelMeanRMSE2)
+# Ποιο μοντέλο είχε το χαμηλότερο μέσο τετραγωνικό σφλάμα;
+bestModelIndex<-which( modelMeanRMSE == min(modelMeanRMSE) )
+# Εμφάνιση μοντέλου με το μικρότερο μέσο τετραγωνικό σφάμα δηλαδή τη μεγαλύτερη ακρίβεια
+print( sprintf("Model with best accuracy was: [%s] error: [%f]", predictionModels[bestModelIndex], all[bestModelIndex]) )
+# Tα μοντέλα παλινδρόμηνσης με το  μέσο τετραγωνικό σφάλμα χρησιμοποιώντας την μέθοδο OLS,όπου
 #εκτιμώνται οι συντελεστές του λαμβάνοντας υπόψη ολόκληρο το σύνολο δεδομένων ως σύνολο εκπαίδευσης
-linear.model2<-lm( as.formula(predictionModels[k]), data=forestfires2 )
-linear.model2
+final.linear.model<-lm( as.formula(predictionModels[bestModelIndex]), data=forestfires )
+final.linear.model
+final.linear.model<-lm( as.formula(predictionModels[bestModelIndex]), data=forestfires2 )
+final.linear.model
